fca690f77db31e5bedc41884ed5c815704621a7d
Saptarshi Ghosh <sapta15@gmail.com>
2023-12-15 15:17:40 +0100

    updated ENCODE communication Pin declarations

--- teensy_backend/include/constants_bioamp.h
+++ teensy_backend/include/constants_bioamp.h
@@ -3,6 +3,9 @@
 
 #define SERIAl_PORT_NAME "/dev/ttyACM0"
 
+// DECODER
+// Teensy -> BioAMP => SPI -? AERC and BIASGEN
+
 #define BIASGEN_MOSI_PIN                11
 #define BIASGEN_SCK_PIN                 13
 #define BIASGEN_ENABLE_PIN              10
@@ -14,18 +17,34 @@
 #define SPI1_ENABLE_PIN                 10
 #define SPI1_RESET_PIN                  41
 
-
-// DECODER
-// Teensy -> BioAMP => SPI -? AERC and BIASGEN
-
-//???
-
-
 // ENCODER
 // BioAmp -> Teensy => Digital Pins
 
 // ENCODER COMMUNICATION
 #define ENCODER_INPUT_SAVE_FILENAME_CSV "../imgui_frontend/data/recordings/untitled.csv"
+
+#define ENCODER1_ACK 24
+#define ENCODER1_REQ 25
+#define ENCODER1_INPUT_NO_PIN 5
+
+//Not sue if these are relevant or not in current context
+#define ENCODER1_HANDSHAKE_ACTIVE_LOW true
+#define ENCODER1_DATA_ACTIVE_LOW false
+#define ENCODER1_DELAY 50
+
+constexpr int ENCODER1_bus_pins[ENCODER1_INPUT_NO_PIN] = {26, 27, 28, 29, 30};
+
+#define ENCODER2_ACK 6
+#define ENCODER2_REQ 5
+#define ENCODER2_INPUT_NO_PIN 5
+
+//Not sue if these are relevant or not in current context
+#define ENCODER2_HANDSHAKE_ACTIVE_LOW true
+#define ENCODER2_DATA_ACTIVE_LOW false
+#define ENCODER2_DELAY 50
+
+constexpr int ENCODER2_bus_pins[ENCODER2_INPUT_NO_PIN] = {4, 3, 2, 1, 0};
+
 
 // #ifdef EXISTS_ENCODER_1
 //     #define ENCODER_ACK 24
@@ -52,8 +71,6 @@
 //     #define ENCODER_INPUT_BIT_4_PIN 0
 // #endif
 
-constexpr int ENCODER_1_pins[] = {24, 25, 5, 26, 27, 28, 29, 30, true, false, 50};
-constexpr int ENCODER_2_pins[] = {6, 5, 5, 4, 3, 2, 1, 0};
   
 
 // RESET PINS
